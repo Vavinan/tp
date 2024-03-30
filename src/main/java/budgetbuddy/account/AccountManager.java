0b5f04edac337622309de010afe3f580e79283b7
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-30 18:48:20 +0800

    Add account creation handling to AccountManager

--- src/main/java/budgetbuddy/account/AccountManager.java
+++ src/main/java/budgetbuddy/account/AccountManager.java
@@ -1,2 +1,17 @@
-package budgetbuddy.account;public class AccountManager {
+package budgetbuddy.account;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+    public ArrayList<Account> accounts;
+    private int accountCount = 0;
+
+    public AccountManager() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(String accountName, double initialBalance) {
+        accountCount ++;
+        accounts.add(new Account(accountCount, accountName, initialBalance));
+    }
 }
