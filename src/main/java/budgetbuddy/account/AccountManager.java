c4245e6a14c10eb9a0f17a8223119b9b018d6cd3
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-31 15:10:10 +0800

    Add processAddAccount method to AccountManager class

--- src/main/java/budgetbuddy/account/AccountManager.java
+++ src/main/java/budgetbuddy/account/AccountManager.java
@@ -1,5 +1,10 @@
 package budgetbuddy.account;
 
+import budgetbuddy.exceptions.EmptyArgumentException;
+import budgetbuddy.exceptions.InvalidArgumentSyntaxException;
+import budgetbuddy.parser.Parser;
+import budgetbuddy.ui.UserInterface;
+
 import java.util.ArrayList;
 
 public class AccountManager {
@@ -10,8 +15,22 @@ public class AccountManager {
         this.accounts = new ArrayList<>();
     }
 
-    public void addAccount(String accountName, double initialBalance) {
+    public void addAccount(String name, double initialBalance) {
         accountCount ++;
-        accounts.add(new Account(accountCount, accountName, initialBalance));
+        accounts.add(new Account(accountCount, name, initialBalance));
+    }
+
+    public void processAddAccount(String input)
+            throws InvalidArgumentSyntaxException, NumberFormatException, EmptyArgumentException {
+
+        String[] arguments = {"/n/", "/$/"};
+        for (String argument: arguments) {
+            if (!input.contains(argument)) {
+                throw new InvalidArgumentSyntaxException("Invalid add account syntax.");
+            }
+        }
+        String[] parsedData = Parser.parseAddAccount(input);
+        addAccount(parsedData[0], Double.parseDouble(parsedData[1]));
+        UserInterface.printAddAccountMessage(accounts.get(accountCount - 1).toString());
     }
 }
