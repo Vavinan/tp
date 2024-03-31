390d501784a166fdbac674355a1e5584154a01d3
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-31 18:02:26 +0800

    Add test cases for fromNumber method

--- src/test/java/budgetbuddy/categories/CategoryTest.java
+++ src/test/java/budgetbuddy/categories/CategoryTest.java
@@ -3,6 +3,7 @@ package budgetbuddy.categories;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CategoryTest {
     @Test
@@ -29,5 +30,19 @@ public class CategoryTest {
         assertEquals(7, Category.RENT.getCategoryNum());
         assertEquals(8, Category.SALARY.getCategoryNum());
         assertEquals(9, Category.OTHERS.getCategoryNum());
+    }
+
+    @Test
+    public void testFromNumber() {
+        assertEquals(Category.DINING, Category.fromNumber(1));
+        assertEquals(Category.GROCERIES, Category.fromNumber(2));
+        assertEquals(Category.UTILITIES, Category.fromNumber(3));
+        assertEquals(Category.TRANSPORTATION, Category.fromNumber(4));
+        assertEquals(Category.HEALTHCARE, Category.fromNumber(5));
+        assertEquals(Category.ENTERTAINMENT, Category.fromNumber(6));
+        assertEquals(Category.RENT, Category.fromNumber(7));
+        assertEquals(Category.SALARY, Category.fromNumber(8));
+        assertEquals(Category.OTHERS, Category.fromNumber(9));
+        assertNull(Category.fromNumber(10)); // Non-existing number
     }
 }
