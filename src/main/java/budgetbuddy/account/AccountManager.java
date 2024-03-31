72fb89a84f7b1373cf6cbc9f9f476d932f1728dc
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-31 22:49:48 +0800

    Add removeAccount to AccountManager

--- src/main/java/budgetbuddy/account/AccountManager.java
+++ src/main/java/budgetbuddy/account/AccountManager.java
@@ -2,6 +2,7 @@ package budgetbuddy.account;
 
 import budgetbuddy.exceptions.EmptyArgumentException;
 import budgetbuddy.exceptions.InvalidArgumentSyntaxException;
+import budgetbuddy.exceptions.InvalidIndexException;
 import budgetbuddy.parser.Parser;
 import budgetbuddy.ui.UserInterface;
 
@@ -9,30 +10,33 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class AccountManager {
+    public static final int INDEX_OFFSET = 1;
+    public static final int LOWER_BOUND = 0;
+
     public ArrayList<Account> accounts;
-    private ArrayList<Integer> existingIdList;
+    private final ArrayList<Integer> existingAccountNumbers;
     private int accountCount = 0;
 
     public AccountManager() {
         this.accounts = new ArrayList<>();
-        this.existingIdList = new ArrayList<>();
+        this.existingAccountNumbers = new ArrayList<>();
     }
 
     public void addAccount(String name, double initialBalance) {
         accountCount ++;
-        int newId = generateId();
-        accounts.add(new Account(newId, name, initialBalance));
-        existingIdList.add(newId);
+        int newAccountNumber = generateAccountNumber();
+        accounts.add(new Account(newAccountNumber, name, initialBalance));
+        existingAccountNumbers.add(newAccountNumber);
     }
 
-    public int generateId() {
+    public int generateAccountNumber() {
         Random random = new Random();
         boolean noMatchFound = true;
         int fourDigitNumber;
         do {
             fourDigitNumber = 1000 + random.nextInt(9000);
-            for (int id: existingIdList) {
-                if (id == fourDigitNumber) {
+            for (int accountNumber: existingAccountNumbers) {
+                if (accountNumber == fourDigitNumber) {
                     noMatchFound = false;
                     break;
                 }
@@ -54,5 +58,20 @@ public class AccountManager {
         String[] parsedData = Parser.parseAddAccount(input);
         addAccount(parsedData[0], Double.parseDouble(parsedData[1]));
         UserInterface.printAddAccountMessage(accounts.get(accountCount - 1).toString());
+    }
+
+    public void removeAccount(String input)
+            throws NumberFormatException, InvalidArgumentSyntaxException, EmptyArgumentException,
+            InvalidIndexException {
+        int deleteId = Parser.parseRemoveAccount(input) - INDEX_OFFSET;
+        int size = accounts.size();
+        if (deleteId >= LOWER_BOUND && deleteId < size) {
+            String accountRemoved = accounts.get(deleteId).toString();
+            accounts.remove(deleteId);
+            accountCount--;
+            UserInterface.printDeleteAccountMessage(accountRemoved);
+        } else {
+            throw new InvalidIndexException(String.valueOf(size));
+        }
     }
 }
