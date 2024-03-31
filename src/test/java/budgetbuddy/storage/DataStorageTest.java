231507926c703c683ad54d4aded1b86f17820f57
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-31 18:16:35 +0800

    Add test cases for method readFromFile

--- src/test/java/budgetbuddy/storage/DataStorageTest.java
+++ src/test/java/budgetbuddy/storage/DataStorageTest.java
@@ -10,8 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DataStorageTest {
     @Test
@@ -29,6 +28,30 @@ public class DataStorageTest {
             assertTrue(file.exists()); // Check if file exists after saving transactions
         } catch (IOException e) {
             fail("Exception thrown while saving transactions: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testReadFromFile() {
+        DataStorage dataStorage = new DataStorage();
+        ArrayList<Transaction> expectedTransactions = new ArrayList<>();
+        Transaction t = new Expense("Groceries", 50.0, "25-03-2024", new Account(1));
+        t.setCategory(Category.fromNumber(1));
+        expectedTransactions.add(t);
+        try {
+            ArrayList<Transaction> actualTransactions = dataStorage.readFileContents();
+
+            // Check if read transactions match the expected transactions
+            assertEquals(expectedTransactions.size(), actualTransactions.size());
+            for (int i = 0; i < expectedTransactions.size(); i++) {
+                assertEquals(expectedTransactions.get(i).getDescription(), actualTransactions.get(i).getDescription());
+                assertEquals(expectedTransactions.get(i).getAmount(), actualTransactions.get(i).getAmount(), 0.001);
+                assertEquals(expectedTransactions.get(i).getDate(), actualTransactions.get(i).getDate());
+                assertEquals(expectedTransactions.get(i).getTransactionType(), actualTransactions.get(i).getTransactionType());
+                assertEquals(expectedTransactions.get(i).getCategory(), actualTransactions.get(i).getCategory());
+            }
+        } catch (IOException e) {
+            fail("Exception thrown while reading from file: " + e.getMessage());
         }
     }
 }
