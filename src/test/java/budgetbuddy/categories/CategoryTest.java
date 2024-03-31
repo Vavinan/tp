e18628fd8c23e09e82b0c09eafd06708d2c8fd19
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-31 18:01:36 +0800

    Add test cases for getCategoryNum method

--- src/test/java/budgetbuddy/categories/CategoryTest.java
+++ src/test/java/budgetbuddy/categories/CategoryTest.java
@@ -17,4 +17,17 @@ public class CategoryTest {
         assertEquals("Salary", Category.SALARY.getCategoryName());
         assertEquals("Others", Category.OTHERS.getCategoryName());
     }
+
+    @Test
+    public void testGetCategoryNum() {
+        assertEquals(1, Category.DINING.getCategoryNum());
+        assertEquals(2, Category.GROCERIES.getCategoryNum());
+        assertEquals(3, Category.UTILITIES.getCategoryNum());
+        assertEquals(4, Category.TRANSPORTATION.getCategoryNum());
+        assertEquals(5, Category.HEALTHCARE.getCategoryNum());
+        assertEquals(6, Category.ENTERTAINMENT.getCategoryNum());
+        assertEquals(7, Category.RENT.getCategoryNum());
+        assertEquals(8, Category.SALARY.getCategoryNum());
+        assertEquals(9, Category.OTHERS.getCategoryNum());
+    }
 }
