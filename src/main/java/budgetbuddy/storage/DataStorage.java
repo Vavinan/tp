90b0d69b270721f871f5b61a5124e1d011994f58
vibes-863 <vaibhavdpillai863@outlook.com>
2024-04-02 23:08:49 +0800

    Update DataStorage to handle storing of accounts

--- src/main/java/budgetbuddy/storage/DataStorage.java
+++ src/main/java/budgetbuddy/storage/DataStorage.java
@@ -1,10 +1,13 @@
 package budgetbuddy.storage;
 
 import budgetbuddy.account.Account;
+import budgetbuddy.account.AccountManager;
 import budgetbuddy.categories.Category;
+import budgetbuddy.transaction.TransactionList;
 import budgetbuddy.transaction.type.Expense;
 import budgetbuddy.transaction.type.Income;
 import budgetbuddy.transaction.type.Transaction;
+import budgetbuddy.ui.UserInterface;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -18,17 +21,34 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DataStorage {
-    public static final String STORAGE_FILE_PATH = "./data/data.txt";
+    public static final String TRANSACTIONS_FILE_PATH = "./data/transactions.txt";
+    public static final String ACCOUNTS_FILE_PATH = "./data/accounts.txt";
     public static final String FOLDER_PATH = "./data";
 
-    private double balance;
+    public void saveAccounts(ArrayList<Account> accounts) {
+        try {
+            File f = new File(ACCOUNTS_FILE_PATH);
+            if (!f.exists()) {
+                createDataFolderIfNotExists();
+                if (!f.createNewFile()) {
+                    throw new IOException("Failed to create file");
+                }
+            }
+            FileWriter fw = new FileWriter(ACCOUNTS_FILE_PATH, false);
+            for (Account account : accounts) {
+                fw.write(account.getAccountNumber() + " ," + account.getName() + " ," + account.getBalance() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error saving accounts.");
+        }
+    }
 
     public void saveTransactions(ArrayList<Transaction> transactionArrayList) throws IOException {
-        File f = new File(STORAGE_FILE_PATH);
+        File f = new File(TRANSACTIONS_FILE_PATH);
 
         assert f.exists() : "File does not exist";
 
-        FileWriter fw = new FileWriter(STORAGE_FILE_PATH);
         for (Transaction transaction : transactionArrayList) {
             if (transaction == null) {
                 break;
@@ -39,7 +59,7 @@ public class DataStorage {
     }
 
     private static void writeToFile(String stringToWrite) throws IOException {
-        FileWriter fw = new FileWriter(STORAGE_FILE_PATH, true);
+        FileWriter fw = new FileWriter(TRANSACTIONS_FILE_PATH, false);
         fw.write(stringToWrite);
         fw.close();
     }
@@ -48,25 +68,27 @@ public class DataStorage {
         LocalDate date = t.getDate();
         String stringDate = date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
         return t.getDescription() + " ," + t.getCategory().getCategoryNum() + " ,"
-                + t.getTransactionType() + " ," + stringDate + " ," + t.getAmount() + "\n";
+                + t.getTransactionType() + " ," + stringDate + " ," + t.getAmount() + " ," + t.getAccountNumber()
+                + " ," + t.getAccountName()+ "\n";
     }
 
-    private Transaction processData(String s, Account account) {
+    // description, categoryNum, type, date, amount, accountNumber, accountName
+    private Transaction processData(String s) {
         String[] transactionInfo = s.split(" ,");
         int categoryNum = Integer.parseInt(transactionInfo[1]);
 
-        assert transactionInfo.length == 5 : "Invalid transaction information format";
+        assert transactionInfo.length ==  7: "Invalid transaction information format";
         assert transactionInfo[2].equals("Income") || transactionInfo[2].equals("Expense") : "Invalid transaction type";
 
         switch (transactionInfo[2]) {
         case "Income":
-            Income incomeObj = new Income(account.getAccountNumber(), account.getName(), transactionInfo[0],
-                    Double.parseDouble(transactionInfo[4]), transactionInfo[3], account);
+            Income incomeObj = new Income(Integer.parseInt(transactionInfo[5]), transactionInfo[6], transactionInfo[0],
+                    Double.parseDouble(transactionInfo[4]), transactionInfo[3]);
             incomeObj.setCategory(Category.fromNumber(categoryNum));
             return incomeObj;
         case "Expense":
-            Expense expenseObj = new Expense(account.getAccountNumber(), account.getName(), transactionInfo[0],
-                    -Double.parseDouble(transactionInfo[4]), transactionInfo[3], account);
+            Expense expenseObj = new Expense(Integer.parseInt(transactionInfo[5]), transactionInfo[6], transactionInfo[0],
+                    -Double.parseDouble(transactionInfo[4]), transactionInfo[3]);
             expenseObj.setCategory(Category.fromNumber(categoryNum));
             return expenseObj;
         default:
@@ -81,26 +103,78 @@ public class DataStorage {
         }
     }
 
-    public ArrayList<Transaction> readFileContents() throws IOException {
+    public ArrayList<Account> readAccountFile() throws IOException {
+        File f = new File(ACCOUNTS_FILE_PATH);
+        Scanner s = new Scanner(f);
+
+        ArrayList<Account> accounts = new ArrayList<>();
+        while (s.hasNext()) {
+            String[] accountInfo = s.nextLine().split(" ,");
+            assert accountInfo.length == 3 : "Invalid account information format";
+            accounts.add(new Account(Integer.parseInt(accountInfo[0]), accountInfo[1],
+                    Double.parseDouble(accountInfo[2])));
+        }
+        return accounts;
+    }
+
+    public ArrayList<Transaction> readTransactionFile() throws IOException {
         createDataFolderIfNotExists();
-        File f = new File(STORAGE_FILE_PATH);
+        File f = new File(TRANSACTIONS_FILE_PATH);
         if (!f.exists()) {
-            f.createNewFile();
+            if (!f.createNewFile()) {
+                throw new IOException("Failed to create file");
+            }
         }
 
         assert f.exists() : "File does not exist";
 
         Scanner s = new Scanner(f);
         ArrayList<Transaction> transactionList = new ArrayList<>();
-        Account account = new Account(1);
-        for (int i = 0; s.hasNext(); i++) {
-            transactionList.add(processData(s.nextLine(), account));
+        while(s.hasNext()) {
+            transactionList.add(processData(s.nextLine()));
         }
-        balance = account.getBalance();
         return transactionList;
     }
 
-    public double getBalance() {
-        return balance;
+    public AccountManager loadAccounts() {
+        try {
+            File f = new File(ACCOUNTS_FILE_PATH);
+            if (!f.exists()) {
+                createDataFolderIfNotExists();
+                if(!f.createNewFile()) {
+                    throw new IOException("Failed to create file");
+                }
+                return createNewAccountManager();
+            }
+            ArrayList<Account> accounts = readAccountFile();
+            ArrayList<Integer> existingAccountNumbers = new ArrayList<>();
+            for (Account account : accounts) {
+                existingAccountNumbers.add(account.getAccountNumber());
+            }
+            return new AccountManager(accounts, existingAccountNumbers);
+        } catch (IOException e) {
+            System.out.println("Error reading accounts file. Creating new account manager.");
+            return createNewAccountManager();
+        }
+    }
+
+    private AccountManager createNewAccountManager() {
+        System.out.println("Let's first create an account for you! What do you want to call it?");
+        String accountName = UserInterface.in.nextLine();
+        System.out.println("Great! What's the initial balance?");
+        double initialBalance = Double.parseDouble(UserInterface.in.nextLine());
+        AccountManager accountManager = new AccountManager();
+        accountManager.addAccount(accountName, initialBalance);
+        return accountManager;
+    }
+
+    public TransactionList loadTransactions() {
+        try {
+            ArrayList<Transaction> transactions = readTransactionFile();
+            return new TransactionList(transactions);
+        } catch (IOException e) {
+            System.out.println("Error reading transactions file. Creating new transaction list.");
+            return new TransactionList();
+        }
     }
 }
