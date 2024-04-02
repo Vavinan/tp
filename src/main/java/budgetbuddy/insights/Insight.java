27c7f6e77bc02bd13deb14d5e6d104ced7865a27
Vavinan <e0959114@u.nus.edu>
2024-04-02 22:10:31 +0800

    Rename chart identifiers for clarity

--- src/main/java/budgetbuddy/insights/Insight.java
+++ src/main/java/budgetbuddy/insights/Insight.java
@@ -15,9 +15,8 @@ import java.util.ArrayList;
 
 import static java.lang.Math.abs;
 
-//@@author ShyamKrishna33
 public class Insight {
-
+    //@@author ShyamKrishna33
     public static void displayCategoryInsight(ArrayList<Transaction> transactionArrayList) {
         Category[] categoryArray = Category.values();
         Double[] expenseArray =  new Double[categoryArray.length];
@@ -39,6 +38,7 @@ public class Insight {
         displayPieChart(categoryArray, incomeArray, expenseArray);
     }
 
+    //@@author
     private static void displayPieChart(Category[] categoryArray, Double[] incomeArray, Double[] expenseArray) {
         JFrame incomeFrame = new JFrame("Income Insights");
         incomeFrame.setLayout(new BorderLayout());
@@ -50,46 +50,47 @@ public class Insight {
         JPanel expensePanel = new JPanel();
         expenseFrame.add(expensePanel, BorderLayout.CENTER);
 
-        PieChart chart1 = new PieChartBuilder().width(800).height(600).title("Income Divide").build();
+        PieChart incomeChart = new PieChartBuilder().width(800).height(600).title("Income Divide").build();
 
         // Customize Chart
-        chart1.getStyler().setCircular(true);
-        chart1.getStyler().setLegendVisible(true);
-        chart1.getStyler().setAnnotationType(PieStyler.AnnotationType.LabelAndPercentage);
+        incomeChart.getStyler().setCircular(true);
+        incomeChart.getStyler().setLegendVisible(true);
+        incomeChart.getStyler().setAnnotationType(PieStyler.AnnotationType.LabelAndPercentage);
 
         for (int i = 0; i < categoryArray.length; i++) {
             if (incomeArray[i] != 0) {
-                chart1.addSeries(categoryArray[i].getCategoryName(), incomeArray[i]);
+                incomeChart.addSeries(categoryArray[i].getCategoryName(), incomeArray[i]);
             }
         }
 
         // Show it
-        incomePanel.add(new XChartPanel<>(chart1));
+        incomePanel.add(new XChartPanel<>(incomeChart));
         incomeFrame.pack();
         incomeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         incomeFrame.setVisible(true);
 
-        PieChart chart2 = new PieChartBuilder().width(800).height(600).title("Expense Divide").build();
+        PieChart expenseChart = new PieChartBuilder().width(800).height(600).title("Expense Divide").build();
 
         // Customize Chart
-        chart2.getStyler().setCircular(true);
-        chart2.getStyler().setLegendVisible(true);
-        chart2.getStyler().setAnnotationType(PieStyler.AnnotationType.LabelAndPercentage);
+        expenseChart.getStyler().setCircular(true);
+        expenseChart.getStyler().setLegendVisible(true);
+        expenseChart.getStyler().setAnnotationType(PieStyler.AnnotationType.LabelAndPercentage);
 
         for (int i = 0; i < categoryArray.length; i++) {
             if (expenseArray[i] != 0) {
-                chart2.addSeries(categoryArray[i].getCategoryName(), expenseArray[i]);
+                expenseChart.addSeries(categoryArray[i].getCategoryName(), expenseArray[i]);
             }
         }
 
         // Show it
-        expensePanel.add(new XChartPanel<>(chart2));
+        expensePanel.add(new XChartPanel<>(expenseChart));
         expenseFrame.pack();
         expenseFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         expenseFrame.setVisible(true);
 
     }
 
+    //@@author ShyamKrishna33
     private static int indexOf(Category[] array, Category target) {
         for (int i = 0; i < array.length; i++) {
             if (array[i].equals(target)) {
