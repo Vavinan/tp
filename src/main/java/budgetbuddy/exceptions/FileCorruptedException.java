67d2c8b6b9fc47f25e9596f3a97fb4a7fbfbb32d
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-04-03 18:34:05 +0800

    Create FileCorruptedException.java

---
+++ src/main/java/budgetbuddy/exceptions/FileCorruptedException.java
@@ -0,0 +1,9 @@
+package budgetbuddy.exceptions;
+
+//@@author ShyamKrishna33
+public class FileCorruptedException extends Exception {
+    public FileCorruptedException(String message) {
+        super(message);
+    }
+}
+//@@author
